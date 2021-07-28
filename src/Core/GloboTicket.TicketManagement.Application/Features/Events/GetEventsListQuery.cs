5a26b5544f7614123424e3f5decced45b9e4e957
Javier Soto <javislop@gmail.com>
2021-07-28 15:47:08 -0400

    add feature events

---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQuery.cs
@@ -0,0 +1,10 @@
+using MediatR;
+using System.Collections.Generic;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+  public class GetEventsListQuery : IRequest<List<EventListViewModel>>
+  {
+
+  }
+}
---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -0,0 +1,29 @@
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using GloboTicket.TicketManagement.Domain.Entities;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using AutoMapper;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+  public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListViewModel>>
+  {
+    private readonly IAsyncRepository<Event> _eventRepository;
+    private readonly IMapper _mapper;
+
+    public GetEventsListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+    {
+      _mapper = mapper;
+      _eventRepository = eventRepository;
+    }
+
+    public async Task<List<EventListViewModel>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+    {
+      var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+      return _mapper.Map<List<EventListViewModel>>(allEvents);
+    }
+  }
+}
